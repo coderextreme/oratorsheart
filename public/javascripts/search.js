785200d68baba02480a469f611b3a51851d13364
John Carlson <yottzumm@gmail.com>
2017-11-03 13:48:57 -0400

    Attempted to add facebook, deleted unused files

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -9,7 +9,7 @@ $('#form').submit(function(e) {
 	});
 });
 
-function getId(url) {
+function getYouTubeId(url) {
     var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
     var match = url.match(regExp);
 
@@ -21,16 +21,18 @@ function getId(url) {
 }
 
 function displayVideo(video) {
-	var videoId = getId(video["Link"]);
+	var videoId = getYouTubeId(video["Link"]);
 	if (videoId === 'error') {
-		videoId = video["Link"];
+		videoId = 'https://www.facebook.com/0caa26a9-2921-4d85-80f5-f1fdea7a8e5f'.replace(/\//g, '%2F').replace(/:/g, '%3A');
 	} else {
 		videoId = '//www.youtube.com/embed/'+videoId;
 	}
 	if (typeof videoId !== 'undefined' && videoId !== '') {
 		$("#table").append('<div class="row">'+
-				'<span class="cell"><iframe width="420" height="315" src="'+
-				videoId+'" frameborder="0" allowfullscreen></iframe></span>'+
+				(videoId.match(/facebook/) ?
+					'<span class="cell"><iframe src="https://www.facebook.com/plugins/video.php?href='+videoId+'&width=420&show_text=false&height=0&appId" width="420" height="315" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe></sapn'
+				: '<span class="cell"><iframe width="420" height="315" src="'+
+				videoId+'" frameborder="0" allowfullscreen></iframe></span>')+
 				'<div class="table">'+
 				'<span class="row">'+video["ID"]+'</span>&nbsp;'+
 				'<span class="row">'+video["Title"]+'</span>&nbsp;'+
