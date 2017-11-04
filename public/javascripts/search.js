41505f8a20aca528167185dc6e79a00f955579e6
John Carlson <yottzumm@gmail.com>
2017-11-04 05:52:02 -0400

    fixed typo

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -31,7 +31,7 @@ function displayVideo(video) {
 		var row = $("#table")
 			.append('<div class="row">')
 		if (videoId.match(/facebook/)) {
-			var left = row.apepnd('<span class="cell"><iframe src="https://www.facebook.com/plugins/video.php?href='+encodeURI(videoId)+'&width=420&show_text=false&height=0&appId" width="420" height="315" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe></sapn'
+			var left = row.append('<span class="cell"><iframe src="https://www.facebook.com/plugins/video.php?href='+encodeURI(videoId)+'&width=420&show_text=false&height=0&appId" width="420" height="315" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe></sapn'
 		} else {
 			var left = row.append('<span class="cell"><iframe width="420" height="315" frameborder="0" allowfullscreen></iframe></span>'))
 			left.attr("src", videoId);
