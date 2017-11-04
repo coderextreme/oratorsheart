eb7b75f13e1d1e41356f61b5dad88a34fd893c8f
John Carlson <yottzumm@gmail.com>
2017-11-04 06:20:41 -0400

    more changes

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -4,7 +4,7 @@ $('#form').submit(function(e) {
 	$("#table").empty();
 	$.post("/search", $(this).serialize(), function(videos) {
 		for (video in videos) {
-			displayVideo(videos[video]);
+			displayVideo(video, videos[video]);
 		}
 	});
 });
@@ -20,7 +20,7 @@ function getYouTubeId(url) {
     }
 }
 
-function displayVideo(video) {
+function displayVideo(v, video) {
 	var videoId = getYouTubeId(video["Link"]);
 	if (videoId === 'error') {
 		videoId = 'https://www.facebook.com/0caa26a9-2921-4d85-80f5-f1fdea7a8e5f'.replace(/\//g, '%2F').replace(/:/g, '%3A');
@@ -28,23 +28,23 @@ function displayVideo(video) {
 		videoId = '//www.youtube.com/embed/'+videoId;
 	}
 	if (typeof videoId !== 'undefined' && videoId !== '') {
-		$('<div id="row" class="row">').appendTo("#table");
+		$('<div id="row'+v+'" class="row">').appendTo($("#table"));
 		if (videoId.match(/facebook/)) {
-			$('<span class="cell"><iframe src="https://www.facebook.com/plugins/video.php?href='+encodeURI(videoId)+'&width=420&show_text=false&height=0&appId" width="420" height="315" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe></sapn').appendTo("#row");
+			$('<span class="cell"><iframe src="https://www.facebook.com/plugins/video.php?href='+encodeURI(videoId)+'&width=420&show_text=false&height=0&appId" width="420" height="315" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe></sapn').appendTo($("#row"+v));
 		} else {
-			$('<span id="left" class="cell">').appendTo("#row");
-			$('<iframe id="iframe" width="420" height="315" frameborder="0" allowfullscreen>').appendTo("#left");
+			$('<span id="left'+v+'" class="cell">').appendTo($("#row"+v));
+			$('<iframe id="iframe'+v+'" width="420" height="315" frameborder="0" allowfullscreen>').appendTo($("#left"+v));
 			$("#iframe").attr("src", videoId);
 		}
-		var right = row.append('<div class="table">');
-		var cell = right.append('<span class="row">');
-		row.text(video["ID"]+"&nbsp;");
-		cell = right.append('<span class="row">');
-		cell.text(video["Title"]+"&nbsp;");
-		cell = right.append('<span class="row">');
-		cell.text(video["Date on Link"]+"&nbsp;");
-		cell = right.append('<span class="row">');
-		cell.text(video["Author/Orator"]+"&nbsp;");
+		var right = $("#row"+v).append('<div class="table">');
+		var cell = $(right).append('<span class="row">');
+		$(cell).text(video["ID"]+"&nbsp;");
+		cell = $(right).append('<span class="row">');
+		$(cell).text(video["Title"]+"&nbsp;");
+		cell = $(right).append('<span class="row">');
+		$(cell).text(video["Date on Link"]+"&nbsp;");
+		cell = $(right).append('<span class="row">');
+		$(cell).text(video["Author/Orator"]+"&nbsp;");
 
 	}
 	return false;
