9cf36e7cf2c1d5f84bdd5bc19cd391cee340b002
John Carlson <yottzumm@gmail.com>
2019-03-26 01:46:53 -0500

    Upgraded from Jade to Pug

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var search = require('./routes/search');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 // app.engine('html', require('ejs').renderFile);
 
 // uncomment after placing your favicon in /public
